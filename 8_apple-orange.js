092fcd199fd1a7d8dce6160aa835e4fc6431cfa6
arfan36 <arfanur36@gmail.com>
2022-08-03 19:56:33 +0600

    18-8 Subtraction, average...

---
+++ 8_apple-orange.js
@@ -0,0 +1,8 @@
+var moneygiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+console.log('Total Expense:', totalPrice);
+var getBack = moneygiven - totalPrice;
+console.log('Return Money Form Shoper:', getBack);
